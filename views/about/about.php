a87e3260515f2c0635fdb00b174693a7654c6000
Satish Kumar Mukkoju <mukkojusatish@gmail.com>
2015-02-06 19:38:04 +0530

    about and contact pages styling added

--- views/about/about.php
+++ views/about/about.php
@@ -1,9 +1,17 @@
 <html>
     <head>
-        <title>Contact</title>
+        <title>About</title>
         <link rel="stylesheet" href="/public/css/index.css" />
+        <link rel="stylesheet" href="/public/css/menu.css" />
+        <link href='http://fonts.googleapis.com/css?family=Quattrocento:400,700' rel='stylesheet' type='text/css'>
     </head>
     <body style="background-color: rgb(231, 231, 231); padding: 0; margin: 0">
-        <div class="cntr"><div class="hdr">About</div></div>
+        
+                            
+        
+        <div class="cntr"><div class="hdr">About</div>
+            <h4>About Us</h4>
+            <!--<p>Twitter and Facebook have changed the dynamics of information distribution. With the real-time reach of these social networks, we believe journalism is on the verge of a paradigm shift. Journalism is no longer about just production and distribution of information. We see journalism as a service through which one can build and engage communities to solve the challenges of the world. Twitter and Facebook have changed the dynamics of information distribution. With the real-time reach of these social networks, we believe journalism is on the verge of a paradigm shift. Journalism is no longer about just production and distribution of information. We see journalism as a service through which one can build and engage communities to solve the challenges of the world.</p>-->
+            </div>
     </body>
 </html>
