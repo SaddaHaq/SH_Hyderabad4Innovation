b5bf9fd1f85caac357438a7fb47ba3f100e44d42
Satish Kumar Mukkoju <mukkojusatish@gmail.com>
2015-02-11 14:28:30 +0530

    about us page content added

--- views/about/about.php
+++ views/about/about.php
@@ -1,2 +1,11 @@
 <?php require 'views/header.php'; ?>
+<div class="cntr">
+            <!--<div class="hdr">Contact</div>-->
+            <div class="abt-s">
+                            <h4>ABOUT US</h4>
+                            <p>Hyderabad4Innovation (H4I) is an informal group that includes the Directors of IIT/IIIT/BITS/ISB and industry leaders like BVR Mohan Reddy, Srini Raju and JA Chowdary and TiE& HYSEA presidents. H4I's objective is to nurture, accelerate and amplify the startup innovation ecosystem in Hyderabad.</p>
+                            <p>Hyderabad 4 Innovation will build a directory of academic incubators, social groups, research institutions, and other commercial entities (incubators, co-working spaces, seed funds & accelerators). Over 80 startup support organizations/groups coming together to deliberate on creating a platform for active collaboration amongst the groups</p>
+            </div>
+            
+        </div>
 <?php require 'views/footer.php'; ?>
